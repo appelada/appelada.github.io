3f96fa15a9ba9b875081c8f35f8b9dbe5de9fed5
Rogerio Lino <rogeriolino@gmail.com>
2026-04-22 22:19:33 -0300

    feat: images

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("js");
+
   return {
     dir: {
       input: ".",
